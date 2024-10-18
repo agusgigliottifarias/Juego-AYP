8ee7c5776fc1dee80cad6c5d18817221a4456ae8
Ckalay5 <makutnich@gmail.com>
2024-10-18 18:17:16 -0300

    Code

---
+++ Moises.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/*
+function interfaz {
+
+}
+*/
+
+int ranking () { //función para mostrar el ranking
+
+}
+
+int jugador () {
+    char nombre;
+    printf ("Ingrese su nombre: %s", nombre);
+    scanf (&nombre);
+    printf ("Bienvenido %s", nombre);
+    printf ("El juego consiste en lo siguiente, adivina la palabra tenes un maximo de 5 intentos, suerte :)")
+    return 0;
+}
+
+int palabra () { // función para seleccionar la palabra del archivo .txt; de no usar el archivo harcodear para tener una cantidad de palabras
+
+}
+
+int cant_letras () { // función para analisar la cantidad de letras de la palabra seleccionada
+
+}
+
+int intentos () { // función para los intentos restantes
+
+}
+
+int munieco () { // función para el muñeco, ACLARACION no es importante
+
+}
+
+int letra () { // función analisis de letra
+
+}
+
+int verificacion () { // función corroboración de letra
+
+}
+
+int resultado () { //funcion en caso de perder el juego
+
+}
+
+//int victoria () { // función en caso de ganar el juego
+
+}
